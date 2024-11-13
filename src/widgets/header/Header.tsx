2d59c6b14ea20c851de804bbbd4e094454e5f8f6
Julia <veluat@gmail.com>
2024-11-13 18:52:06 +0300

    feat: add Header

---
+++ src/widgets/header/Header.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { Button } from '@internshipsamyrai44-ui-kit/components-lib';
+import styles from './Header.module.scss';
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <h2>Pictorial Header</h2>
+      <Button variant={'outlined'}>Sign In</Button>
+    </header>
+  );
+}
