0f86bcc757a1ef1b9d1624fa85ddc525d2496721
Julia <veluat@gmail.com>
2024-11-13 15:19:08 +0300

    refactor: apply favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,24 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import React from 'react'
+import type { Metadata } from 'next';
+import './globals.css';
+import React from 'react';
+import Head from 'next/head';
 
 export const metadata: Metadata = {
-  title: "Pictorial",
-  description: "Pictorial Next App",
+  title: 'Pictorial',
+  description: 'Pictorial Next App'
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body>{children}</body>
     </html>
   );
 }
