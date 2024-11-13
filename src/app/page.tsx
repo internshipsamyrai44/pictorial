886f3194abbb7be7c2fdabde1a189a88a9eef265
Julia <veluat@gmail.com>
2024-11-13 18:49:21 +0300

    refactor: change structure of the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+'use client';
+
+import React from 'react';
+import { Card } from '@internshipsamyrai44-ui-kit/components-lib';
+import Header from '@/widgets/header/Header';
+import styles from './page.module.scss';
+
 export default function Home() {
-  return <div>Public page</div>;
+  return (
+    <>
+      <Header />
+      <Card className={styles.card}>
+        <span>Public page</span>
+      </Card>
+    </>
+  );
 }
