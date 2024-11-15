1eb7a571be4bf4e5e4307538ee0b1ad6c114a863
Julia <veluat@gmail.com>
2024-11-15 16:26:59 +0300

    fix: update next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,33 @@
 import type { NextConfig } from 'next';
+import { webpack } from 'next/dist/compiled/webpack/webpack';
+import Configuration = webpack.Configuration;
+
+const regexEqual = (x: RegExp, y: RegExp): boolean => {
+  return (
+    x instanceof RegExp &&
+    y instanceof RegExp &&
+    x.source === y.source &&
+    x.global === y.global &&
+    x.ignoreCase === y.ignoreCase &&
+    x.multiline === y.multiline
+  );
+};
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config: Configuration) => {
+    const oneOf = config.module.rules.find((rule) => typeof rule.oneOf === 'object');
+    if (oneOf) {
+      const sassRule = oneOf.oneOf.find((rule) => regexEqual(rule.test, /\.module\.(scss|sass)$/));
+      if (sassRule) {
+        const sassLoader = sassRule.use.find((el) => el.loader.includes('next/dist/compiled/sass-loader'));
+        if (sassLoader) {
+          sassLoader.loader = 'sass-loader';
+        }
+      }
+    }
+
+    return config;
+  }
 };
 
 export default nextConfig;
