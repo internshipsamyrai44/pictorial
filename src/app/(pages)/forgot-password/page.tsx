8f0f5297e4a4abc83bf630b49cee5578f1e1562b
fanur56 <ilasovf@gmail.com>
2024-11-21 14:20:59 +0300

    forgot password page created

--- src/app/(pages)/forgot-password/page.tsx
+++ src/app/(pages)/forgot-password/page.tsx
@@ -1,3 +1,14 @@
+import Link from 'next/link';
+
 export default function ForgotPassword() {
-  return <div>Forgot Password</div>;
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+      <p>Forgot Password</p>
+      <span>Email</span>
+      <input type="text" />
+      <span>Enter your email address and we will send you further instructions </span>
+      <button style={{ cursor: 'pointer', backgroundColor: 'blue' }}>Send Link</button>
+      <Link href={'/login'}>Back to Sign In</Link>
+    </div>
+  );
 }
