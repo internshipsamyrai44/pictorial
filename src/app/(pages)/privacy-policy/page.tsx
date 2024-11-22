16417ad61a93ebe2f9957c129e1055eed4d4360b
Julia <veluat@gmail.com>
2024-11-22 22:05:01 +0300

    feat: add PrivacyPolicy

--- src/app/(pages)/privacy-policy/page.tsx
+++ src/app/(pages)/privacy-policy/page.tsx
@@ -1,3 +1,13 @@
+import React from 'react';
+import { Privacy } from '@/shared/ui/privacy/Privacy';
+import s from './privacyPage.module.scss';
+import { Button } from '@internshipsamyrai44-ui-kit/components-lib';
+
 export default function PrivacyPolicy() {
-  return <div>Privacy Policy</div>;
+  return (
+    <div className={s.container}>
+      <Button variant={'outlined'} title={'Back to Sign in'} />
+      <Privacy title={'Privacy Policy'} />
+    </div>
+  );
 }
