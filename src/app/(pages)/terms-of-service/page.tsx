a8fc399ba8d1dd414a797d5aa77f9749ea6b8130
Julia <veluat@gmail.com>
2024-12-11 13:52:48 +0300

    feat: add Terms of Service

--- src/app/(pages)/terms-of-service/page.tsx
+++ src/app/(pages)/terms-of-service/page.tsx
@@ -1,13 +1,17 @@
+'use client';
+
 import Header from '@/widgets/header/Header';
 import s from './termsOfService.module.scss';
+import { Button } from '@internshipsamyrai44-ui-kit/components-lib';
+import Terms from '@/shared/ui/terms/Terms';
 
 export default function TermsOfService() {
   return (
     <>
       <Header />
       <div className={s.container}>
-        <button>Back to Sign in</button>
-        <Terms title={'Terms Of Service'} />
+        <Button variant={'ghost'}>Back to Sign in </Button>
+        <Terms />
       </div>
     </>
   );
