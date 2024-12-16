5a2c2dcd779593914c7971688dac5c75f1a1e679
Julia <veluat@gmail.com>
2024-12-16 23:11:05 +0300

    feat: add Terms of Service

--- src/shared/ui/terms/Terms.tsx
+++ src/shared/ui/terms/Terms.tsx
@@ -1,57 +1,44 @@
+import { Typography } from '@internshipsamyrai44-ui-kit/components-lib';
+
 export default function Terms() {
   return (
     <div>
-      Terms of services
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Fames ac turpis egestas integer eget aliquet nibh. Amet consectetur adipiscing elit ut aliquam
-        purus sit amet luctus. Tortor vitae purus faucibus ornare suspendisse sed nisi. Dolor sit amet consectetur
-        adipiscing. Massa enim nec dui nunc mattis enim ut tellus. Scelerisque eleifend donec pretium vulputate sapien
-        nec sagittis. Feugiat nisl pretium fusce id velit ut tortor pretium viverra. Tortor aliquam nulla facilisi cras.
-        Elit pellentesque habitant morbi tristique senectus et netus. Nulla facilisi nullam vehicula ipsum a arcu
-        cursus. Ut lectus arcu bibendum at varius vel pharetra. Etiam erat velit scelerisque in dictum non consectetur.
-        Quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. Id diam maecenas ultricies mi eget mauris
-        pharetra. Tincidunt lobortis feugiat vivamus at augue. Non odio euismod lacinia at. Aliquet eget sit amet
-        tellus. Auctor neque vitae tempus quam. Tellus in hac habitasse platea dictumst vestibulum rhoncus est. Nisl
-        pretium fusce id velit ut tortor pretium. Eget arcu dictum varius duis at consectetur. Est placerat in egestas
-        erat imperdiet sed euismod nisi porta. Scelerisque felis imperdiet proin fermentum. Tellus in hac habitasse
-        platea dictumst vestibulum rhoncus. Proin nibh nisl condimentum id venenatis a condimentum vitae. Massa
-        tincidunt dui ut ornare lectus sit amet est placerat. Vel turpis nunc eget lorem dolor sed viverra ipsum. Enim
-        ut tellus elementum sagittis. At consectetur lorem donec massa sapien faucibus et molestie. Enim sit amet
-        venenatis urna cursus. Id velit ut tortor pretium viverra suspendisse potenti. Et magnis dis parturient montes
-        nascetur ridiculus. Donec ultrices tincidunt arcu non sodales neque. Malesuada fames ac turpis egestas maecenas
-        pharetra convallis posuere. Consequat mauris nunc congue nisi vitae. Ut tellus elementum sagittis vitae et leo
-        duis. Dignissim sodales ut eu sem. Cras semper auctor neque vitae tempus quam pellentesque nec nam. Et magnis
-        dis parturient montes nascetur ridiculus mus mauris. Morbi tempus iaculis urna id. Tristique nulla aliquet enim
-        tortor. Libero nunc consequat interdum varius sit. Sed adipiscing diam donec adipiscing tristique risus nec
-        feugiat. Facilisi nullam vehicula ipsum a arcu cursus vitae. Arcu odio ut sem nulla pharetra. Tincidunt praesent
-        semper feugiat nibh sed pulvinar proin. Morbi non arcu risus quis varius quam quisque id diam. Ac turpis egestas
-        sed tempus urna. Sit amet venenatis urna cursus eget nunc. Amet consectetur adipiscing elit ut aliquam purus
-        sit. Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Et egestas quis ipsum suspendisse ultrices
-        gravida dictum. Ligula ullamcorper malesuada proin libero nunc consequat interdum varius sit. Pretium viverra
-        suspendisse potenti nullam ac tortor vitae. Lobortis elementum nibh tellus molestie nunc non blandit. Eget nunc
-        lobortis mattis aliquam. Quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus. Quis commodo odio
-        aenean sed adipiscing diam. Pharetra massa massa ultricies mi quis hendrerit. Et magnis dis parturient montes.
-        Cursus metus aliquam eleifend mi in nulla posuere. Tristique senectus et netus et. A lacus vestibulum sed arcu
-        non odio. Sed elementum tempus egestas sed. Faucibus et molestie ac feugiat sed lectus vestibulum mattis
-        ullamcorper. Gravida rutrum quisque non tellus orci ac. Turpis tincidunt id aliquet risus feugiat in ante metus
-        dictum. Dolor morbi non arcu risus quis varius quam quisque. Bibendum at varius vel pharetra vel turpis nunc. A
-        cras semper auctor neque. Aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant. Erat
-        nam at lectus urna duis. Mauris pharetra et ultrices neque. Nulla facilisi etiam dignissim diam quis enim
-        lobortis scelerisque fermentum. Justo nec ultrices dui sapien eget mi proin sed libero. Pellentesque sit amet
-        porttitor eget dolor. A diam maecenas sed enim ut. Fermentum et sollicitudin ac orci phasellus. Tincidunt vitae
-        semper quis lectus nulla. Tincidunt dui ut ornare lectus sit amet est. Sed enim ut sem viverra aliquet eget sit.
-        Eu augue ut lectus arcu bibendum at varius. Suspendisse sed nisi lacus sed viverra tellus in hac habitasse.
-        Donec ultrices tincidunt arcu non sodales neque sodales ut etiam. Varius vel pharetra vel turpis nunc eget lorem
-        dolor. Arcu odio ut sem nulla pharetra. Bibendum neque egestas congue quisque. Facilisis volutpat est velit
-        egestas dui. Orci nulla pellentesque dignissim enim sit amet. Mauris vitae ultricies leo integer malesuada nunc
-        vel risus commodo. Odio ut enim blandit volutpat maecenas volutpat blandit. Eu mi bibendum neque egestas congue
-        quisque egestas diam in. Neque viverra justo nec ultrices dui sapien. Congue mauris rhoncus aenean vel elit.
-        Vitae aliquet nec ullamcorper sit. Tempus imperdiet nulla malesuada pellentesque. Sed lectus vestibulum mattis
-        ullamcorper velit. Commodo odio aenean sed adipiscing diam. Viverra nam libero justo laoreet. Id neque aliquam
-        vestibulum morbi blandit cursus. Vel facilisis volutpat est velit egestas dui id ornare. Feugiat nibh sed
-        pulvinar proin.
-      </p>
+      <Typography as={'h1'} variant={'h1'}>
+        Terms of services
+      </Typography>
+      <Typography>
+        Welcome to Pictorial Site! These Terms of Service (Terms) govern your use of our application and services
+        (Services). By accessing or using our Services, you agree to these Terms. If you do not agree to these Terms,
+        please do not use our Services. <br />
+        1. Acceptance of Terms By creating an account or using our Services, you confirm that you are at least 13 years
+        old and have the legal capacity to enter into these Terms. If you are using the Services on behalf of a company
+        or organization, you represent that you have the authority to bind that entity to these Terms. <br />
+        2. Changes to Terms We may update these Terms from time to time. We will notify you of any changes by posting
+        the new Terms on this page. You are advised to review these Terms periodically for any changes. Your continued
+        use of the Services after changes have been made constitutes your acceptance of the new Terms. <br />
+        3. User Accounts To access certain features of our Services, you may need to create an account. You agree to
+        provide accurate, current, and complete information during the registration process and to update such
+        information to keep it accurate, current, and complete. You are responsible for maintaining the confidentiality
+        of your account and password and for restricting access to your account. <br />
+        4. Use of Services You agree to use the Services only for lawful purposes and in accordance with these Terms.
+        You agree not to: Use the Services in any way that violates any applicable federal, state, local, or
+        international law or regulation. Engage in any conduct that restricts or inhibits anyone&#39;s use or enjoyment
+        of the Services. Use any data mining, robots, or similar data gathering or extraction methods. <br />
+        5. Intellectual Property The Services and their original content, features, and functionality are and will
+        remain the exclusive property of [Your Company Name] and its licensors. The Services are protected by copyright,
+        trademark, and other laws of both the United States and foreign countries. 6. Third-Party Links Our Services may
+        contain links to third-party websites or services that are not owned or controlled by Pictorial Site. We have no
+        control over, and assume no responsibility for, the content, privacy policies, or practices of any third-party
+        websites or services. <br />
+        7. Disclaimer of Warranties Our Services are provided on an &#34;as is&#34; and &#34;as available&#34; basis.
+        Pictorial Site makes no warranties, whether express or implied, regarding the Services, including but not
+        limited to their reliability, availability, or suitability for your needs. 8. Limitation of Liability In no
+        event shall Pictorial Site, its directors, employees, partners, agents, suppliers, or affiliates be liable for
+        any indirect, incidental, special, special, consequential, or punitive damages, including without limitation
+        loss of profits, data, use, goodwill, or other intangible losses, resulting from: Your access to or use of (or
+        inability to access or use) the Services. Any conduct or content of any third party on the Services. Any content
+        obtained from the Services. Unauthorized access, use, or alteration of your transmissions or content.
+      </Typography>
     </div>
   );
 }
