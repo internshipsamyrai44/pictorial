5b0fa3da351d822bfb86cad0ec2d6b0d46c80b5f
Vlad Razvenkov <dssdom@gmail.com>
2024-12-20 22:27:23 +0100

    fix: routing

--- src/shared/const/PATH.ts
+++ src/shared/const/PATH.ts
@@ -1,12 +1,12 @@
 export const PATH = {
   MAIN: '/',
-  LOGIN: 'auth/login',
-  SIGNUP: 'auth/signup',
-  VERIFICATION_LINK_EXPIRED: 'auth/verification-link-expired',
-  FORGOT_PASSWORD: 'auth/forgot-password',
-  RECOVERY: 'auth/recovery',
-  PRIVACY_POLICY: 'auth/privacy-policy',
-  REGISTRATION_CONFIRMATION: 'auth/registration-confirmation',
-  TERMS_OF_SERVICE: 'auth/terms-of-service',
+  LOGIN: '/auth/login',
+  SIGNUP: '/auth/signup',
+  VERIFICATION_LINK_EXPIRED: '/auth/verification-link-expired',
+  FORGOT_PASSWORD: '/auth/forgot-password',
+  RECOVERY: '/auth/recovery',
+  PRIVACY_POLICY: '/auth/privacy-policy',
+  REGISTRATION_CONFIRMATION: '/auth/registration-confirmation',
+  TERMS_OF_SERVICE: '/auth/terms-of-service',
   PROFILE: '/profile'
 };
