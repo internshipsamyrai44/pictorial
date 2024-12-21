51a19a1726a0c10f2450dd0c55bdcec93ef8490b
fanur56 <ilasovf@gmail.com>
2024-12-21 23:28:18 +0300

    fix: get base url

--- src/shared/utils/getBaseUrl.ts
+++ src/shared/utils/getBaseUrl.ts
@@ -1,7 +1,7 @@
 export const getBaseUrl = () => {
-  if (process.env.NODE_ENV === 'production') return `${window.location.protocol}//${window.location.host}`;
+  if (process.env.NODE_ENV === 'development') return `${window.location.protocol}//${window.location.host}`;
 
-  if (process.env.NODE_ENV === 'development') return 'https://pictorial.work';
+  if (process.env.NODE_ENV === 'production') return 'https://pictorial.work';
 
   return 'test';
 };
