5104fd8465309e13a2f5e425bb66e9ae077ab20b
Jeff <dssdom@gmail.com>
2024-12-27 01:37:16 +0100

    feat: private and public routes

---
+++ src/middleware.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { PATH } from './shared/const/PATH';
+
+export function middleware(request: NextRequest) {
+  const publicPaths = [
+    PATH.LOGIN,
+    PATH.FORGOT_PASSWORD,
+    PATH.REGISTRATION_CONFIRMATION,
+    PATH.TERMS_OF_SERVICE,
+    PATH.PRIVACY_POLICY,
+    PATH.RECOVERY,
+    PATH.MAIN,
+    PATH.SIGNUP,
+    PATH.VERIFICATION_LINK_EXPIRED
+  ];
+  const { pathname } = request.nextUrl;
+
+  if (publicPaths.includes(pathname)) {
+    return NextResponse.next();
+  }
+
+  const token = request.cookies.get('accessToken');
+
+  if (!token) {
+    return NextResponse.redirect(new URL(PATH.LOGIN, request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+};
