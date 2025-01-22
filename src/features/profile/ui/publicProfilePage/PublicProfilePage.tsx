7b871eb8a5d0636d4f56fd38ce8ffdc47230a797
fanur56 <ilasovf@gmail.com>
2025-01-22 23:14:50 +0300

    fix: PublicProfilePage

--- src/features/profile/ui/publicProfilePage/PublicProfilePage.tsx
+++ src/features/profile/ui/publicProfilePage/PublicProfilePage.tsx
@@ -4,9 +4,9 @@ import s from './PublicProfilePage.module.scss';
 import { Alertpopup, Button, LoaderLinear } from '@internshipsamyrai44-ui-kit/components-lib';
 import Image from 'next/image';
 import { useGetPublicUserProfileQuery } from '@/features/profile/api/publicProfileApi';
-import noAvatar from '../../../../../public/images/noAvatar.png';
 import { useParams } from 'next/navigation';
 import { useRequestError } from '@/shared/hooks/useRequestError';
+import { ProfileDashboard } from '@/widgets/profile-dashboard/ProfileDashboard';
 
 export const PublicProfilePage = () => {
   const { id } = useParams<{ id: string }>();
@@ -18,57 +18,31 @@ export const PublicProfilePage = () => {
   return (
     <>
       {errorMessage && <Alertpopup alertType={'error'} message={errorMessage} />}
-      <div className={s.container}>
-        <div className={s.profile}>
+      <ProfileDashboard
+        avatar={data?.avatars[0]?.url}
+        userFollowers={data?.userMetadata.followers || 0}
+        userFollowing={data?.userMetadata.following || 0}
+        userName={data?.userName || 'no info'}
+        userPublications={data?.userMetadata.publications || 0}
+      >
+        <div className={s.buttons}>
+          <Button>Follow</Button> {/*TODO follow logic*/}
+          <Button variant={'secondary'}>Send Message</Button> {/*TODO send message logic*/}
+        </div>
+      </ProfileDashboard>
+      <div className={s.images}>
+        {data?.avatars.map((el, index) => (
           <Image
-            className={s.avatar}
-            loader={() => data?.avatars[0].url || ''}
-            src={data?.avatars[0]?.url || noAvatar}
-            priority={true}
-            width={0}
+            className={s.image}
+            loader={() => el.url}
             height={0}
-            alt={'Avatar image'}
+            width={0}
+            key={index}
+            src={el.url}
+            alt={'Profile images'}
             unoptimized={true}
           />
-          <div className={s.content}>
-            <div className={s.info}>
-              <div className={s.name}>{data?.userName}</div>
-              <div className={s.buttons}>
-                <Button>Follow</Button> {/*TODO follow logic*/}
-                <Button variant={'secondary'}>Send Message</Button> {/*TODO send message logic*/}
-              </div>
-            </div>
-            <div className={s.statistic}>
-              <div>
-                <div>{data?.userMetadata.following}</div>
-                <span>Following</span>
-              </div>
-              <div>
-                <div>{data?.userMetadata.followers}</div>
-                <span>Followers</span>
-              </div>
-              <div>
-                <div>{data?.userMetadata.publications}</div>
-                <span>Publications</span>
-              </div>
-            </div>
-            <div>{data?.aboutMe}</div>
-          </div>
-        </div>
-        <div className={s.images}>
-          {data?.avatars.map((el, index) => (
-            <Image
-              className={s.image}
-              loader={() => el.url}
-              height={0}
-              width={0}
-              key={index}
-              src={el.url}
-              alt={'Profile images'}
-              unoptimized={true}
-            />
-          ))}
-        </div>
+        ))}
       </div>
     </>
   );
