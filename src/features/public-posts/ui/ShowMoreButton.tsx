fb702235a7dd1892090a17a0a5dc6486ba8971e5
Artem Rybin <catobazzz@gmail.com>
2025-01-30 11:01:03 +0300

    feat: add btn ShowMoreButton.tsx

---
+++ src/features/public-posts/ui/ShowMoreButton.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+
+import s from './ShowMoreButton.module.scss';
+
+export type Props = {
+  maxLength: number;
+  text: string;
+};
+
+export const ShowMoreButton = ({ maxLength, text }: Props) => {
+  const [isTrimmed, setIsTrimmed] = useState(true);
+  const toggleReadMore = () => {
+    setIsTrimmed(!isTrimmed);
+  };
+
+  if (text.length <= maxLength) {
+    return <>{text}</>;
+  }
+
+  const trimmedText = `${text.slice(0, maxLength)}...`;
+  const textToShow = isTrimmed ? trimmedText : text;
+  const buttonText = isTrimmed ? 'Show more' : 'Hide';
+
+  return (
+    <>
+      {textToShow}
+      <button className={s.button} onClick={toggleReadMore}>
+        {buttonText}
+      </button>
+    </>
+  );
+};
