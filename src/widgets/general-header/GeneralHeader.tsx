46adde8f85f967e05ba90bb4ff23baf3d3242871
Artem Rybin <catobazzz@gmail.com>
2025-02-09 15:09:39 +0300

    ref: GeneralHeader.tsx GeneralHeader.module.scss

--- src/widgets/general-header/GeneralHeader.tsx
+++ src/widgets/general-header/GeneralHeader.tsx
@@ -1,13 +1,59 @@
 'use client';
 
-import { Header } from '@internshipsamyrai44-ui-kit/components-lib';
+import Link from 'next/link';
 import s from './GeneralHeader.module.scss';
+import {
+  Button,
+  FlagRussiaIcon,
+  FlagUnitedKingdomIcon,
+  Select,
+  SelectItem
+} from '@internshipsamyrai44-ui-kit/components-lib';
 
-export const GeneralHeader = () => {
-  return (
-    <div className={s.wrapper}>
-      {/* СДЕЛАТЬ ТОГИКУ ДЛЯ ХЕДЕРА */}
-      <Header />
-    </div>
-  );
+type GeneralHeaderProps = {
+  isAuth: boolean;
 };
+
+export const GeneralHeader = ({ isAuth = false }: GeneralHeaderProps) => (
+  <header className={s.wrapper}>
+    <div className={s.container}>
+      <Link href="/" className={s.logo}>
+        Inctagram
+      </Link>
+      <div className={s.notificationContainer}>
+        <SelectTranslate />
+        {!isAuth && <HeaderButton />}
+      </div>
+    </div>
+  </header>
+);
+
+const HeaderButton = () => (
+  <div className={s.buttonContainer}>
+    <Button className={s.logIn} asChild variant="ghost">
+      <Link href="/auth/login">Log in</Link>
+    </Button>
+    <Button asChild variant="primary" className={s.signUp}>
+      <Link href="/auth/signup">Sign up</Link>
+    </Button>
+  </div>
+);
+
+const SelectTranslate = () => (
+  <div className={s.select}>
+    <Select defaultValue="en">
+      <SelectItem value="en">
+        <div className={s.selectValue}>
+          <FlagUnitedKingdomIcon />
+          <span>English</span>
+        </div>
+      </SelectItem>
+      <SelectItem value="ru">
+        <div className={s.selectValue}>
+          <FlagRussiaIcon />
+          <span>Русский</span>
+        </div>
+      </SelectItem>
+    </Select>
+  </div>
+);
