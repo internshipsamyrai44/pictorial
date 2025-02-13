eb73a857e0d6ad21584b5420b2b7b552fad42759
dyv21 <denis@artkerr.com>
2025-02-13 21:27:28 +0300

    feat: add AvatarActions component

--- src/features/profile/ui/settings/general-info/avatar-actions/AvatarActions.tsx
+++ src/features/profile/ui/settings/general-info/avatar-actions/AvatarActions.tsx
@@ -1,5 +1,25 @@
 import s from './AvatarActions.module.scss';
+import Image from 'next/image';
+
+import { Button } from '@internshipsamyrai44-ui-kit/components-lib';
+import defaultPic from '././../../../../../../../public/icons/PicIcon.svg';
+import { useRef } from 'react';
 
 export const AvatarActions = () => {
-  return <div className={s.container}>Avatar Actions</div>;
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleButtonClick = () => {
+    fileInputRef.current?.click();
+  };
+  return (
+    <div className={s.container}>
+      <div className={s.pic} onClick={handleButtonClick}>
+        <Image src={defaultPic} alt={'User profile pic'} />
+      </div>
+      <input type="file" id="avatar-input" accept="image/*" ref={fileInputRef} className={'hidden'} />
+      <Button variant={'outlined'} onClick={handleButtonClick}>
+        Add a Profile Photo
+      </Button>
+    </div>
+  );
 };
