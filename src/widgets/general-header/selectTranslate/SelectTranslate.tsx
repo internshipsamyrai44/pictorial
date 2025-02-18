e6dad738f034a5d94fb7c143d66f3a73a8ba6d08
metedri <mitedria@gmail.com>
2025-02-18 18:28:30 +0300

    added language switching to SelectTranslate

--- src/widgets/general-header/selectTranslate/SelectTranslate.tsx
+++ src/widgets/general-header/selectTranslate/SelectTranslate.tsx
@@ -1,21 +1,37 @@
+import { setUserLocale } from '@/i18n/locale';
 import s from '@/widgets/general-header/GeneralHeader.module.scss';
 import { FlagRussiaIcon, FlagUnitedKingdomIcon, Select, SelectItem } from '@internshipsamyrai44-ui-kit/components-lib';
+import { useLocale } from 'next-intl';
+import { useTransition } from 'react';
 
-export const SelectTranslate = () => (
-  <div className={s.select}>
-    <Select defaultValue="en">
-      <SelectItem value="en">
-        <div className={s.selectValue}>
-          <FlagUnitedKingdomIcon />
-          <span>English</span>
-        </div>
-      </SelectItem>
-      <SelectItem value="ru">
-        <div className={s.selectValue}>
-          <FlagRussiaIcon />
-          <span>Русский</span>
-        </div>
-      </SelectItem>
-    </Select>
-  </div>
-);
+export const SelectTranslate = () => {
+  const locale = useLocale();
+  // eslint-disable-next-line no-unused-vars
+  const [isPending, startTransition] = useTransition();
+
+  const handleChange = (value: string) => {
+    const locale = value as 'en' | 'ru';
+    startTransition(() => {
+      setUserLocale(locale);
+    });
+  };
+
+  return (
+    <div className={s.select}>
+      <Select defaultValue={locale} onValueChange={handleChange}>
+        <SelectItem value="en">
+          <div className={s.selectValue}>
+            <FlagUnitedKingdomIcon />
+            <span>English</span>
+          </div>
+        </SelectItem>
+        <SelectItem value="ru">
+          <div className={s.selectValue}>
+            <FlagRussiaIcon />
+            <span>Русский</span>
+          </div>
+        </SelectItem>
+      </Select>
+    </div>
+  );
+};
