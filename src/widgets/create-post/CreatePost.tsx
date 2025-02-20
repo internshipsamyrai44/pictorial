6897c11e4b527629b3a6fd7eda6d971f4cec37e8
dyv21 <denis@artkerr.com>
2025-02-20 20:26:39 +0300

    feat: upload user photo handler

--- src/widgets/create-post/CreatePost.tsx
+++ src/widgets/create-post/CreatePost.tsx
@@ -3,7 +3,7 @@ import { Button, Modal } from '@internshipsamyrai44-ui-kit/components-lib';
 import * as React from 'react';
 import defaultPic from '../../../public/icons/PicIcon.svg';
 import Image from 'next/image';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 type PropsType = {
   // eslint-disable-next-line no-unused-vars
@@ -13,28 +13,55 @@ type PropsType = {
 export const CreatePost = (props: PropsType) => {
   const { setCreatePostActive } = props;
   const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const [userPhoto, setuserPhoto] = useState<string | null>(null);
 
   const handleButtonClick = () => {
     fileInputRef.current?.click();
   };
 
+  const uploadUserPhotoHandler = (e: any) => {
+    const userPhotoFile = e.target.files?.[0];
+    if (userPhotoFile) {
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        if (reader.result) {
+          setuserPhoto(reader.result as string);
+        }
+      };
+
+      reader.readAsDataURL(userPhotoFile);
+    }
+  };
+
   return (
-    <Modal title={'Add Photo'} className={s.wrapper} onClose={() => setCreatePostActive(false)}>
-      <div className={s.image} onClick={handleButtonClick}>
-        <Image src={defaultPic} alt={'Default Photo'} />
+    <Modal
+      title={`${userPhoto ? 'Cropping' : 'Add Photo'} `}
+      className={s.wrapper}
+      onClose={() => setCreatePostActive(false)}
+    >
+      {userPhoto ? (
+        <Image src={userPhoto} alt={'User Photo'} layout="responsive" width={100} height={100} />
+      ) : (
+        <div className={s.image} onClick={handleButtonClick}>
+          <Image src={defaultPic} alt={'Default Photo'} />
+        </div>
+      )}
+
+      <input
+        type="file"
+        accept="image/jpeg, image/png"
+        id="photo-loading"
+        ref={fileInputRef}
+        className={s.input}
+        onChange={uploadUserPhotoHandler}
+      />
+      <div className={`${s.buttons} ${userPhoto ? s.hidden : ''}`}>
+        <Button variant={'primary'} onClick={handleButtonClick}>
+          Select from Computer
+        </Button>
+        <Button variant={'outlined'}>Open Draft</Button>
       </div>
-      <input type="file" accept="image/jpeg, image/png" id="photo-loading" ref={fileInputRef} className={s.input} />
-      <Button variant={'primary'} onClick={handleButtonClick}>
-        Select from Computer
-      </Button>
-      <Button
-        variant={'outlined'}
-        onClick={() => {
-          console.log('Open Draft');
-        }}
-      >
-        Open Draft
-      </Button>
     </Modal>
   );
 };
