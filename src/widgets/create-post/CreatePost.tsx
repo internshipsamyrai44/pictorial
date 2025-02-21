c056ea744394e9bebda0c0409068a25e9f290414
dyv21 <denis@artkerr.com>
2025-02-21 12:06:10 +0300

    feat: separate CreatePost into screens component

--- src/widgets/create-post/CreatePost.tsx
+++ src/widgets/create-post/CreatePost.tsx
@@ -1,83 +1,25 @@
 import s from './CreatePost.module.scss';
-import { Button, Modal } from '@internshipsamyrai44-ui-kit/components-lib';
+import { Modal } from '@internshipsamyrai44-ui-kit/components-lib';
 import * as React from 'react';
-import defaultPic from '../../../public/icons/PicIcon.svg';
-import Image from 'next/image';
-import { ChangeEvent, useRef, useState } from 'react';
+
+import { useState } from 'react';
+import { Startlayout } from '@/widgets/create-post/Start-layout/Startlayout';
 
 type PropsType = {
   // eslint-disable-next-line no-unused-vars
   setCreatePostActive: (value: boolean) => void;
 };
+
 export const CreatePost = (props: PropsType) => {
   const { setCreatePostActive } = props;
-  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
   const [userPhoto, setUserPhoto] = useState<string | null>(null);
-  const MAX_SIZE = 20 * 1024 * 1024;
-
-  const imageCheck = (file: File) => {
-    if (file.size > MAX_SIZE) {
-      alert('Please upload a file smaller than 20MB!');
-      return;
-    }
-    if (!file.type.startsWith('image/jpeg') && !file.type.startsWith('image/png')) {
-      alert('Please upload JPEG or PNG image format!');
-      return;
-    }
-  };
-
-  const handleButtonClick = () => {
-    fileInputRef.current?.click();
-  };
-
-  const uploadUserPhotoHandler = (e: ChangeEvent<HTMLInputElement>) => {
-    const userPhotoFile = e.target.files?.[0];
-
-    if (userPhotoFile) {
-      imageCheck(userPhotoFile as File);
-
-      const reader = new FileReader();
-      reader.onerror = () => {
-        alert(`Uploading file Error! ${reader.error}`);
-      };
-
-      reader.onloadend = () => {
-        if (reader.result) {
-          setUserPhoto(reader.result as string);
-        }
-      };
-      reader.readAsDataURL(userPhotoFile);
-    }
-  };
 
   return (
-    <Modal
-      title={`${userPhoto ? 'Cropping' : 'Add Photo'} `}
-      className={s.wrapper}
-      onClose={() => setCreatePostActive(false)}
-    >
-      {userPhoto ? (
-        <Image src={userPhoto} alt={'User Photo'} layout="responsive" width={100} height={100} />
-      ) : (
-        <div className={s.image} onClick={handleButtonClick}>
-          <Image src={defaultPic} alt={'Default Photo'} />
-        </div>
-      )}
-
-      <input
-        type="file"
-        accept="image/jpeg, image/png"
-        id="photo-loading"
-        ref={fileInputRef}
-        className={s.input}
-        onChange={uploadUserPhotoHandler}
-      />
-      <div className={`${s.buttons} ${userPhoto ? s.hidden : ''}`}>
-        <Button variant={'primary'} onClick={handleButtonClick}>
-          Select from Computer
-        </Button>
-        `<Button variant={'outlined'}>Open Draft</Button>
-      </div>
+    <Modal title={'Add Photo'} className={s.wrapper} onClose={() => setCreatePostActive(false)}>
+      <Startlayout userPhoto={userPhoto} setUserPhoto={setUserPhoto} />
     </Modal>
   );
 };
+
+//<Image src={userPhoto} alt={'User Photo'} layout="responsive" width={100} height={100} />
---
+++ src/widgets/create-post/Start-layout/Startlayout.tsx
@@ -0,0 +1,58 @@
+import s from './StartLayot.module.scss';
+import Image from 'next/image';
+// eslint-disable-next-line import/no-unresolved
+import defaultPic from '../../../../public/icons/PicIcon.svg';
+import { Button } from '@internshipsamyrai44-ui-kit/components-lib';
+import * as React from 'react';
+import { useRef } from 'react';
+
+type PropsType = {
+  // eslint-disable-next-line no-unused-vars
+  setUserPhoto: (photo: string | null) => void;
+  userPhoto: string | null;
+};
+
+export const Startlayout = (props: PropsType) => {
+  const { setUserPhoto } = props;
+  const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleButtonClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  const uploadUserPhotoHandler = (e: any) => {
+    const userPhotoFile = e.target.files?.[0];
+    if (userPhotoFile) {
+      const reader = new FileReader();
+
+      reader.onloadend = () => {
+        if (reader.result) {
+          setUserPhoto(reader.result as string);
+        }
+      };
+      reader.readAsDataURL(userPhotoFile);
+    }
+  };
+  ``;
+  return (
+    <>
+      <div className={s.image} onClick={handleButtonClick}>
+        <Image src={defaultPic} alt={'Default Photo'} />
+      </div>
+      <input
+        type="file"
+        accept="image/jpeg, image/png"
+        id="photo-loading"
+        ref={fileInputRef}
+        className={s.input}
+        onChange={uploadUserPhotoHandler}
+      />
+      <div className={`${s.buttons}`}>
+        <Button variant={'primary'} onClick={handleButtonClick}>
+          Select from Computer
+        </Button>
+        <Button variant={'outlined'}>Open Draft</Button>
+      </div>
+    </>
+  );
+};
