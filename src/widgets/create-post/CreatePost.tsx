9aa51d98e8048631db3653291908579c8cfd3d91
dyv21 <denis@artkerr.com>
2025-02-21 10:06:59 +0300

    feat: add user Photo check

--- src/widgets/create-post/CreatePost.tsx
+++ src/widgets/create-post/CreatePost.tsx
@@ -3,33 +3,47 @@ import { Button, Modal } from '@internshipsamyrai44-ui-kit/components-lib';
 import * as React from 'react';
 import defaultPic from '../../../public/icons/PicIcon.svg';
 import Image from 'next/image';
-import { useRef, useState } from 'react';
+import { ChangeEvent, useRef, useState } from 'react';
 
 type PropsType = {
   // eslint-disable-next-line no-unused-vars
   setCreatePostActive: (value: boolean) => void;
 };
-
+``;
 export const CreatePost = (props: PropsType) => {
   const { setCreatePostActive } = props;
   const fileInputRef = useRef<HTMLInputElement | null>(null);
-  const [userPhoto, setuserPhoto] = useState<string | null>(null);
+  const [userPhoto, setUserPhoto] = useState<string | null>(null);
+  const MAX_SIZE = 5 * 1024 * 1024;
+
+  const imageCheck = (file: File) => {
+    if (file.size > MAX_SIZE) {
+      alert('Please upload a file smaller than 20MB!');
+      return;
+    }
+    if (!file.type.startsWith('image/jpeg') && !file.type.startsWith('image/png')) {
+      alert('Please upload JPEG or PNG image format!');
+      return;
+    }
+  };
 
   const handleButtonClick = () => {
     fileInputRef.current?.click();
   };
 
-  const uploadUserPhotoHandler = (e: any) => {
+  const uploadUserPhotoHandler = (e: ChangeEvent<HTMLInputElement>) => {
     const userPhotoFile = e.target.files?.[0];
+
     if (userPhotoFile) {
+      imageCheck(userPhotoFile as File);
+
       const reader = new FileReader();
 
       reader.onloadend = () => {
         if (reader.result) {
-          setuserPhoto(reader.result as string);
+          setUserPhoto(reader.result as string);
         }
       };
-
       reader.readAsDataURL(userPhotoFile);
     }
   };
@@ -60,7 +74,7 @@ export const CreatePost = (props: PropsType) => {
         <Button variant={'primary'} onClick={handleButtonClick}>
           Select from Computer
         </Button>
-        <Button variant={'outlined'}>Open Draft</Button>
+        `<Button variant={'outlined'}>Open Draft</Button>
       </div>
     </Modal>
   );
