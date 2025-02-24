2dd5d5a78dde32e54bf822f8adfc0ad91c206ccf
dyv21 <denis@artkerr.com>
2025-02-24 19:27:39 +0300

    feat: separate postAPI types

--- src/features/posts/api/postsApi.ts
+++ src/features/posts/api/postsApi.ts
@@ -1,54 +1,10 @@
 import { inctagramApi } from '@/app/services/inctagram.api';
-
-type PostImageViewModel = {
-  url: string;
-  width: number;
-  height: number;
-  fileSize: number;
-  createdAt: string;
-  uploadId: string;
-};
-
-type UploadedImageViewModel = {
-  images: PostImageViewModel[];
-};
-
-type PostRequestData = {
-  description: string;
-  childrenMetadata: uploadId[];
-};
-
-type uploadId = {
-  uploadId: string | undefined;
-};
-
-type PublishedPostResponse = {
-  id: number;
-  userName: string;
-  description: string;
-  location: string;
-  images: PostImageViewModel[];
-  createdAt: string;
-  updatedAt: string;
-  ownerId: number;
-  avatarOwner: string;
-  owner: UserName;
-  likesCount: number;
-  isLiked: boolean;
-  avatarWhoLikes: boolean;
-};
-
-type PostResponse = {
-  pageSize: number;
-  totalCount: number;
-  notReadCount: number;
-  items: PublishedPostResponse[];
-};
-
-type UserName = {
-  firstName: string;
-  lastName: string;
-};
+import {
+  PostRequestData,
+  PostResponse,
+  PublishedPostResponse,
+  UploadedImageViewModel
+} from '@/features/posts/model/postsApi.types';
 
 export const postsApi = inctagramApi.injectEndpoints({
   endpoints: (build) => ({
---
+++ src/features/posts/model/postsApi.types.ts
@@ -0,0 +1,49 @@
+export type PostImageViewModel = {
+  url: string;
+  width: number;
+  height: number;
+  fileSize: number;
+  createdAt: string;
+  uploadId: string;
+};
+
+export type UploadedImageViewModel = {
+  images: PostImageViewModel[];
+};
+
+export type PostRequestData = {
+  description: string;
+  childrenMetadata: uploadId[];
+};
+
+export type uploadId = {
+  uploadId: string | undefined;
+};
+
+export type PublishedPostResponse = {
+  id: number;
+  userName: string;
+  description: string;
+  location: string;
+  images: PostImageViewModel[];
+  createdAt: string;
+  updatedAt: string;
+  ownerId: number;
+  avatarOwner: string;
+  owner: UserName;
+  likesCount: number;
+  isLiked: boolean;
+  avatarWhoLikes: boolean;
+};
+
+export type PostResponse = {
+  pageSize: number;
+  totalCount: number;
+  notReadCount: number;
+  items: PublishedPostResponse[];
+};
+
+export type UserName = {
+  firstName: string;
+  lastName: string;
+};
