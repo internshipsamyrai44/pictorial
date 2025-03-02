de4c41d42278a8e8b4f3bceed5d011269d107a97
metedri <mitedria@gmail.com>
2025-03-02 13:15:46 +0300

    PP-65: added display of posts and opening a post by click

--- src/features/profile/ui/Profile.tsx
+++ src/features/profile/ui/Profile.tsx
@@ -1,27 +1,17 @@
 'use client';
 
 import * as React from 'react';
-import { useGetUserByUserNameQuery } from '@/features/profile/api/profileApi';
+import { useGetProfileQuery, useGetUserByUserNameQuery } from '@/features/profile/api/profileApi';
 import { ProfileDashboard } from '@/widgets/profile-dashboard/ProfileDashboard';
 import { Button, LoaderLinear } from '@internshipsamyrai44-ui-kit/components-lib';
-import { useGetPublicUserProfileByIdQuery } from '@/features/profile/api/publicProfileApi ';
-import { useParams } from 'next/navigation';
 import Link from 'next/link';
 import { PATH } from '@/shared/const/PATH';
 import { useTranslations } from 'next-intl';
+import ProfilePosts from '@/features/posts/ui/posts/ProfilePosts';
 
 export default function Profile() {
-  const { id } = useParams();
   const t = useTranslations('Profile');
-  const profileId = id ? +id : undefined;
-
-  const { data: profileData, isLoading: isProfileLoading } = useGetPublicUserProfileByIdQuery(
-    profileId !== undefined ? profileId : 0,
-    {
-      skip: profileId === undefined
-    }
-  );
-
+  const { data: profileData, isLoading: isProfileLoading } = useGetProfileQuery();
   const { data: userData, isLoading: isUserDataLoading } = useGetUserByUserNameQuery(profileData?.userName ?? '', {
     skip: !profileData
   });
@@ -31,17 +21,20 @@ export default function Profile() {
   }
 
   return (
-    <ProfileDashboard
-      about={profileData?.aboutMe || t('NoInfo')}
-      avatar={profileData?.avatars[0]?.url}
-      userFollowers={userData?.followersCount || 0}
-      userFollowing={userData?.followingCount || 0}
-      userName={profileData?.userName || t('NoInfo')}
-      userPublications={userData?.publicationsCount || 0}
-    >
-      <Button variant={'secondary'}>
-        <Link href={PATH.PROFILE.SETTINGS}>{t('ProfileSettings')}</Link>
-      </Button>
-    </ProfileDashboard>
+    <>
+      <ProfileDashboard
+        about={profileData?.aboutMe || t('NoInfo')}
+        avatar={profileData?.avatars[0]?.url}
+        userFollowers={userData?.followersCount || 0}
+        userFollowing={userData?.followingCount || 0}
+        userName={profileData?.userName || t('NoInfo')}
+        userPublications={userData?.publicationsCount || 0}
+      >
+        <Button variant={'secondary'}>
+          <Link href={PATH.PROFILE.SETTINGS}>{t('ProfileSettings')}</Link>
+        </Button>
+      </ProfileDashboard>
+      {profileData && <ProfilePosts userName={profileData.userName} />}
+    </>
   );
 }
