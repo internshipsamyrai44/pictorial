dca83728d52a0abf1ec174afc0cefbc3142498f5
dyv21 <denis@artkerr.com>
2025-03-03 18:40:18 +0300

    fix: svg profile default pic

--- src/features/profile/ui/settings/general-info/avatar-actions/AvatarActions.tsx
+++ src/features/profile/ui/settings/general-info/avatar-actions/AvatarActions.tsx
@@ -1,8 +1,7 @@
 import s from './AvatarActions.module.scss';
-import Image from 'next/image';
-
+``;
 import { Button } from '@internshipsamyrai44-ui-kit/components-lib';
-import defaultPic from '././../../../../../../../public/icons/PicIcon.svg';
+import FileIcon from '././../../../../../../../public/icons/PicIcon.svg';
 import { useRef } from 'react';
 import { useTranslations } from 'next-intl';
 
@@ -16,7 +15,7 @@ export const AvatarActions = () => {
   return (
     <div className={s.container}>
       <div className={s.pic} onClick={handleButtonClick}>
-        <Image src={defaultPic} alt={'User profile pic'} />
+        <FileIcon width={48} height={48} />
       </div>
       <input type="file" id="avatar-input" accept="image/*" ref={fileInputRef} className={s.input} />
       <Button variant={'outlined'} onClick={handleButtonClick}>
