384327b1cedd22f0835cb9b5f334376c4f6c3366
dyv21 <denis@artkerr.com>
2025-03-05 08:10:24 +0300

    feat: add setZoomScale function

--- src/features/posts/ui/create-post/ZoomPhoto/ZoomPhoto.tsx
+++ src/features/posts/ui/create-post/ZoomPhoto/ZoomPhoto.tsx
@@ -1,16 +1,28 @@
 import s from './ZoomPhoto.module.scss';
 import { ChangeEvent } from 'react';
+import { useCreatePostContext } from '@/shared/hooks/useCreatePostContext';
 
 type PropsType = {
   zoomValue: string;
   // eslint-disable-next-line no-unused-vars
   setZoomValue: (zoomValue: string) => void;
+  currentPhotoId: string | undefined;
 };
 
 export const ZoomPhoto = (props: PropsType) => {
-  const { zoomValue, setZoomValue } = props;
+  const { userPhotos } = useCreatePostContext();
+  const { zoomValue, setZoomValue, currentPhotoId } = props;
   const onChangeHanler = (e: ChangeEvent<HTMLInputElement>) => {
     setZoomValue(e.target.value);
+    setZoomScale();
+  };
+
+  const setZoomScale = () => {
+    userPhotos.forEach((photo) => {
+      if (photo.id === currentPhotoId) {
+        photo.zoom = zoomValue;
+      }
+    });
   };
 
   return (
