80102fea2777fc369cab40496caec5b930a72931
metedri <mitedria@gmail.com>
2025-03-05 13:45:05 +0300

    feat: added styles for AddComentForm

--- src/features/posts/ui/posts/postModal/postContent/addComentForm/AddComentForm.tsx
+++ src/features/posts/ui/posts/postModal/postContent/addComentForm/AddComentForm.tsx
@@ -1,13 +1,16 @@
 'use client';
 
+import { useTranslations } from 'next-intl';
 import s from './AddComentForm.module.scss';
 import { Button } from '@internshipsamyrai44-ui-kit/components-lib';
 
 export default function AddComentForm() {
+  const t = useTranslations('Post');
+
   return (
     <div className={s.addComent}>
-      <input className={s.input} />
-      <Button variant={'ghost'}>Publish</Button>
+      <input className={s.input} placeholder={t('AddComment')} />
+      <Button variant={'ghost'}>{t('CreatePost.Publish')}</Button>
     </div>
   );
 }
