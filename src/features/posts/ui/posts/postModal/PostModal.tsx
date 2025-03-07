d331bbd22fa24e77b40e7e218c9ffb68e1b400d1
metedri <mitedria@gmail.com>
2025-03-07 13:36:30 +0300

    PP-66/feat: Added skeleton for uploading photo post

--- src/features/posts/ui/posts/postModal/PostModal.tsx
+++ src/features/posts/ui/posts/postModal/PostModal.tsx
@@ -2,7 +2,6 @@
 
 import s from './PostModal.module.scss';
 import { useGetPostsByIdQuery } from '@/features/posts/api/postsApi';
-import { Loader } from '@/shared/ui/loader/Loader';
 import CloseButton from './closeButton/CloseButton';
 import PostImage from './postImage/PostImage';
 import PostContent from './postContent/PostContent';
@@ -22,16 +21,17 @@ export default function PostModal({ postID, closeModal }: Props) {
     <div className={s.wrap}>
       <div className={s.modalContainer}>
         <CloseButton onClick={closeModal} />
-        <div className={s.postContainer}>
-          <div className={s.postImg}>
-            {isLoading && <Loader />}
-            {post && <PostImage images={post.images} />}
+        {isLoading && <PostContentSkeleton />}
+        {post && (
+          <div className={s.postContainer}>
+            <div className={s.postImg}>
+              <PostImage images={post.images} />
+            </div>
+            <div className={s.postContent}>
+              <PostContent post={post} closeModal={closeModal} />
+            </div>
           </div>
-          <div className={s.postContent}>
-            {isLoading && <PostContentSkeleton />}
-            {post && <PostContent post={post} closeModal={closeModal} />}
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
