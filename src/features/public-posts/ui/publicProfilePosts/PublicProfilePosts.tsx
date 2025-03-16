9954e89ca22b9eed8b246144fb4305e6ec714c94
dyv21 <denis@artkerr.com>
2025-03-16 08:44:34 +0300

    feat: add url for public posts

--- src/features/public-posts/ui/publicProfilePosts/PublicProfilePosts.tsx
+++ src/features/public-posts/ui/publicProfilePosts/PublicProfilePosts.tsx
@@ -5,8 +5,7 @@ import { Alertpopup } from '@internshipsamyrai44-ui-kit/components-lib';
 import Image from 'next/image';
 import { useGetPublicUserPostsQuery } from '@/features/public-posts/api/publicPostApi';
 import PostsSkeleton from '@/features/posts/ui/posts/postsSkeleton/PostsSkeleton';
-import { useState } from 'react';
-import PostModal from '@/features/posts/ui/posts/postModal/PostModal';
+import Link from 'next/link';
 
 type Props = {
   id: number;
@@ -15,10 +14,6 @@ type Props = {
 export const PublicProfilePosts = ({ id }: Props) => {
   // Получаем посты для пользователя с id, переданным через пропсы
   const { data: posts, error, isFetching } = useGetPublicUserPostsQuery({ userId: id });
-  const [selectedPostID, setSelectedPostID] = useState<number | null>(null);
-  const openModal = (postID: number) => setSelectedPostID(postID);
-  const closeModal = () => setSelectedPostID(null);
-
   if (error) return <Alertpopup alertType="error" message="Error fetching posts" />;
 
   if (isFetching) {
@@ -31,27 +26,25 @@ export const PublicProfilePosts = ({ id }: Props) => {
 
   // Фильтруем посты, оставляя только те, где есть URL изображения
   const validPosts = posts?.items.filter((post) => post.images?.[0]?.url);
-
   return (
     <div className={s.images}>
       {validPosts && validPosts.length > 0 ? (
         validPosts.map((post) => (
-          <Image
-            key={post.id}
-            className={s.image}
-            loader={() => post.images[0].url}
-            src={post.images[0].url}
-            alt="Profile image"
-            unoptimized={true}
-            height={0}
-            width={0}
-            onClick={() => openModal(post.id)}
-          />
+          <Link key={post.id} href={`/public-user/profile/${post.ownerId}/${post.id}`} className={s.postList}>
+            <Image
+              className={s.image}
+              loader={() => post.images[0].url}
+              src={post.images[0].url}
+              alt="Profile image"
+              unoptimized={true}
+              height={0}
+              width={0}
+            />
+          </Link>
         ))
       ) : (
         <p>No posts available</p>
       )}
-      {selectedPostID && posts && <PostModal postID={selectedPostID} closeModal={closeModal} />}
     </div>
   );
 };
