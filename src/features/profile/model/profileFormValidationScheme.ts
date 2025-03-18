768b9a3014783e4ee88b1e5613253c404a06d64d
metedri <mitedria@gmail.com>
2025-03-18 21:17:15 +0300

    feat: Added correct validation when filling in profile data

--- src/features/profile/model/profileFormValidationScheme.ts
+++ src/features/profile/model/profileFormValidationScheme.ts
@@ -4,29 +4,29 @@ export const profileFormValidationScheme = yup.object({
   userName: yup
     .string()
     .trim()
-    .min(6, 'Username must be at least 6 characters')
-    .max(20, 'Username must be at most 20 characters')
-    .matches(/^[a-zA-Z0-9_-]*$/g, `Username must contain a-z, A-Z, 0-9, -`)
-    .required('Username is required'),
+    .min(6, 'Errors.UserNameMinCharacters')
+    .max(20, 'Errors.UserNameMaxCharacters')
+    .matches(/^[a-zA-Z0-9_-]*$/g, 'Errors.UserNameMatches')
+    .required('Errors.UserNameRequired'),
   firstName: yup
     .string()
     .trim()
-    .min(1, 'First Name must be at least 1 character')
-    .max(50, 'First Name must be at most 50 characters')
-    .matches(/^[a-zA-Zа-яА-Я0-9_-]*$/g, `First Name must contain a-z, A-Z, 0-9, -`)
-    .required('First Name is required'),
+    .min(1, 'Errors.FirstNameMinCharacters')
+    .max(50, 'Errors.FirstNameMaxCharacters')
+    .matches(/^[a-zA-Zа-яА-Я0-9_-]*$/g, `Errors.FirstNameMatches`)
+    .required('Errors.FirstNameRequired'),
   lastName: yup
     .string()
     .trim()
-    .min(1, 'Last Name must be at least 1 character')
-    .max(50, 'Last Name must be at most 50 characters')
-    .matches(/^[a-zA-Zа-яА-Я0-9_-]*$/g, `Last Name must contain a-z, A-Z, 0-9, -`)
-    .required('Last Name is required'),
+    .min(1, 'Errors.LastNameMinCharacters')
+    .max(50, 'Errors.LastNameMaxCharacters')
+    .matches(/^[a-zA-Zа-яА-Я0-9_-]*$/g, `Errors.LastNameMatches`)
+    .required('Errors.LastNameRequired'),
   aboutMe: yup
     .string()
     .trim()
-    .max(200, 'About Me must be at most 200 characters')
-    .matches(/^[a-zA-Zа-яА-Я0-9!#$%&'()*+,\-./:;<=>?@[\\\]^_`{|} ]*$/g, `About Me must contain valid characters`)
+    .max(200, 'Errors.AboutMeMaxCharacters')
+    .matches(/^[a-zA-Zа-яА-Я0-9!#$%&'()*+,\-./:;<=>?@[\\\]^_`{|} ]*$/g, `Errors.AboutMeMatches`)
     .transform((value) => (value === null ? '' : value)),
   city: yup.string().transform((value) => (value === null ? '' : value)),
   country: yup.string().transform((value) => (value === null ? '' : value)),
