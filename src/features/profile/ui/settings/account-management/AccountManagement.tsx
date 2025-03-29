2a24c2b2570c85cb55ffc37b6782fb5100030994
dyv21 <denis@artkerr.com>
2025-03-29 07:01:28 +0300

    feat: add Account type section

--- src/features/profile/ui/settings/account-management/AccountManagement.tsx
+++ src/features/profile/ui/settings/account-management/AccountManagement.tsx
@@ -1,6 +1,23 @@
-import { useTranslations } from 'next-intl';
+import { useState } from 'react';
+import s from './AccountManagement.module.scss';
+import { RadioGroup, Typography } from '@internshipsamyrai44-ui-kit/components-lib';
 
 export const AccountManagement = () => {
-  const t = useTranslations('Profile');
-  return <div>{t('AccountManagementContent')}</div>;
+  // const t = useTranslations('Profile');
+  const [accountType, setAccountType] = useState<string>('personal');
+  const accountTypeOptions = [
+    { label: 'Personal', value: 'personal' },
+    { label: 'Business', value: 'business' }
+  ];
+
+  return (
+    <div className={s.container}>
+      <div className={s.account}>
+        <Typography variant={'h3'}>Account type</Typography>
+        <div className={s.type}>
+          <RadioGroup defaultValue={accountType} onValueChange={setAccountType} options={accountTypeOptions} />
+        </div>
+      </div>
+    </div>
+  );
 };
