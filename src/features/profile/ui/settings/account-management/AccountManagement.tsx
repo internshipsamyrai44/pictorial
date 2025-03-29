43a6dc6dfedfb908ed9c220250ebf5cba2cf91ed
dyv21 <denis@artkerr.com>
2025-03-29 07:08:22 +0300

    feat: add subscription section

--- src/features/profile/ui/settings/account-management/AccountManagement.tsx
+++ src/features/profile/ui/settings/account-management/AccountManagement.tsx
@@ -4,18 +4,30 @@ import { RadioGroup, Typography } from '@internshipsamyrai44-ui-kit/components-l
 
 export const AccountManagement = () => {
   // const t = useTranslations('Profile');
-  const [accountType, setAccountType] = useState<string>('personal');
+  const [accountType, setAccountType] = useState<string>('business');
   const accountTypeOptions = [
     { label: 'Personal', value: 'personal' },
     { label: 'Business', value: 'business' }
+  ];
+  const [subscription, setSubscription] = useState<string>('monthly');
+  const subscriptionOptions = [
+    { label: '$10 per 1 Day', value: 'daily' },
+    { label: '$50 per 7 Day', value: 'weekly' },
+    { label: '$$100 per month', value: 'monthly' }
   ];
 
   return (
     <div className={s.container}>
       <div className={s.account}>
         <Typography variant={'h3'}>Account type</Typography>
-        <div className={s.type}>
+        <div className={s.radio}>
           <RadioGroup defaultValue={accountType} onValueChange={setAccountType} options={accountTypeOptions} />
+        </div>
+      </div>
+      <div className={s.subscription}>
+        <Typography variant={'h3'}>Your subscription costs:</Typography>
+        <div className={s.radio}>
+          <RadioGroup defaultValue={subscription} onValueChange={setSubscription} options={subscriptionOptions} />
         </div>
       </div>
     </div>
