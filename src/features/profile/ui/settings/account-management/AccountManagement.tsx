da58fe703bf1630e981096d3a088a0b8b14ccfa6
dyv21 <denis@artkerr.com>
2025-03-29 11:01:51 +0300

    feat: add current subscription component

--- src/features/profile/ui/settings/account-management/AccountManagement.tsx
+++ src/features/profile/ui/settings/account-management/AccountManagement.tsx
@@ -3,9 +3,11 @@ import s from './AccountManagement.module.scss';
 import { Button, RadioGroup, Typography } from '@internshipsamyrai44-ui-kit/components-lib';
 import StripeIcon from './../../../../../../public/icons/stripeIcon.svg';
 import PayPallIcon from './../../../../../../public/icons/paypallIcon.svg';
+import { CurrentSubscription } from '@/features/profile/ui/settings/account-management/current-subscription/CurrentSubscription';
 
 export const AccountManagement = () => {
   // const t = useTranslations('Profile');
+  const [isSubscribed, setIsSubscribed] = useState(true);
   const [accountType, setAccountType] = useState<string>('personal');
   const accountTypeOptions = [
     { label: 'Personal', value: 'personal' },
@@ -20,16 +22,17 @@ export const AccountManagement = () => {
 
   return (
     <div className={s.container}>
+      {isSubscribed && <CurrentSubscription />}
       <div className={s.account}>
-        <Typography variant={'h3'}>Account type</Typography>
-        <div className={s.radio}>
+        <Typography variant={'h3'}>Account type:</Typography>
+        <div className={s.content}>
           <RadioGroup defaultValue={accountType} onValueChange={setAccountType} options={accountTypeOptions} />
         </div>
       </div>
       {accountType === 'business' && (
-        <div className={s.subscription}>
+        <div className={s.subscribe}>
           <Typography variant={'h3'}>Your subscription costs:</Typography>
-          <div className={s.radio}>
+          <div className={s.content}>
             <RadioGroup defaultValue={subscription} onValueChange={setSubscription} options={subscriptionOptions} />
           </div>
           <div className={s.payment}>
@@ -37,7 +40,7 @@ export const AccountManagement = () => {
               <PayPallIcon />
             </Button>
             or
-            <Button variant={'ghost'} className={s.payment__btn}>
+            <Button variant={'ghost'} className={s.payment__btn} onClick={() => setIsSubscribed(!isSubscribed)}>
               <StripeIcon />
             </Button>
           </div>
