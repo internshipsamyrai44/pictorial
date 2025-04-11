8ab894b79995a3add027b5b904ddc5ae31df9cef
dyv21 <denis@artkerr.com>
2025-04-11 20:20:07 +0300

    refactor: add custom hook useSubscription

--- src/features/subscriptions/ui/PaypalSubscriptionButton/PaypalSubscriptionButton.tsx
+++ src/features/subscriptions/ui/PaypalSubscriptionButton/PaypalSubscriptionButton.tsx
@@ -1,55 +1,38 @@
 'use client';
-import { useState } from 'react';
 import { Button } from '@internshipsamyrai44-ui-kit/components-lib';
 import PaypalIcon from '../../../../../public/icons/paypallIcon.svg';
 import { ConfirmModal } from '@/features/profile/ui/settings/account-management/confirm-modal/ConfimModal';
 import s from './PaypalSubscriptionButton.module.scss';
-import { useCreateSubscriptionMutation } from '@/features/subscriptions/api/subscriptionsApi';
-import { SUBSCRIPTION_PRICES, SUBSCRIPTION_TYPE_MAP } from '@/features/subscriptions/model/subscriptionConstants';
 import { SubscriptionTypes } from '@/features/profile/ui/settings/account-management/subscription-price/SubscriptionPrice';
+import { useSubscription } from '@/shared/hooks/useSubscription';
+import { useState } from 'react';
 
 type Props = {
   chosenSubscription: SubscriptionTypes;
 };
 
 export const PaypalSubscriptionButton = ({ chosenSubscription }: Props) => {
+  const { subscribe, isLoading, paymentUrl } = useSubscription();
   const [showConfirmModal, setShowConfirmModal] = useState(false);
-  const [paymentUrl, setPaymentUrl] = useState('');
+  const paymentMerchant = 'PAYPAL';
 
-  const [createSubscription, { isLoading }] = useCreateSubscriptionMutation();
-
-  const handleSubscribe = async () => {
-    try {
-      const response = await createSubscription({
-        typeSubscription: SUBSCRIPTION_TYPE_MAP[chosenSubscription],
-        paymentType: 'PAYPAL',
-        amount: SUBSCRIPTION_PRICES[chosenSubscription],
-        baseUrl: 'http://localhost:3000/profile/settings?tab=account-management&success=true'
-      }).unwrap();
-
-      if (response?.url) {
-        setPaymentUrl(response.url);
-        setShowConfirmModal(true);
-      }
-    } catch (err) {
-      console.error('Error subscription:', err);
-    }
+  const handleSubscribe = () => {
+    subscribe({ chosenSubscription, paymentMerchant });
+    setShowConfirmModal(true);
   };
   return (
-    <>
-      <div className={s.wrapper}>
-        <Button variant={'ghost'} className={s.payment__btn} onClick={handleSubscribe}>
-          <PaypalIcon />
-        </Button>
-      </div>
+    <div className={s.wrapper}>
+      <Button variant={'ghost'} className={s.payment__btn} onClick={handleSubscribe}>
+        <PaypalIcon />
+      </Button>
       {showConfirmModal && (
         <ConfirmModal
           setShowModal={setShowConfirmModal}
           paymentUrl={paymentUrl}
           isLoading={isLoading}
-          paymentType={'PAYPAL'}
+          paymentMerchant={paymentMerchant}
         />
       )}
-    </>
+    </div>
   );
 };
--- src/features/subscriptions/ui/StripeSubscriptionButton/StripeSubscriptionButton.tsx
+++ src/features/subscriptions/ui/StripeSubscriptionButton/StripeSubscriptionButton.tsx
@@ -4,47 +4,29 @@ import { Button } from '@internshipsamyrai44-ui-kit/components-lib';
 import StripeIcon from '../../../../../public/icons/stripeIcon.svg';
 import { ConfirmModal } from '@/features/profile/ui/settings/account-management/confirm-modal/ConfimModal';
 import s from './StripeSubscriptionButton.module.scss';
-import { useCreateSubscriptionMutation } from '@/features/subscriptions/api/subscriptionsApi';
-import { SUBSCRIPTION_PRICES, SUBSCRIPTION_TYPE_MAP } from '@/features/subscriptions/model/subscriptionConstants';
 import { SubscriptionTypes } from '@/features/profile/ui/settings/account-management/subscription-price/SubscriptionPrice';
+import { useSubscription } from '@/shared/hooks/useSubscription';
 
 type Props = {
   chosenSubscription: SubscriptionTypes;
 };
 
 export const StripeSubscriptionButton = ({ chosenSubscription }: Props) => {
+  const { subscribe, isLoading, paymentUrl } = useSubscription();
   const [showConfirmModal, setShowConfirmModal] = useState(false);
-  const [paymentUrl, setPaymentUrl] = useState('');
 
-  const [createSubscription, { isLoading }] = useCreateSubscriptionMutation();
-
-  const handleSubscribe = async () => {
-    try {
-      const response = await createSubscription({
-        typeSubscription: SUBSCRIPTION_TYPE_MAP[chosenSubscription],
-        paymentType: 'STRIPE',
-        amount: SUBSCRIPTION_PRICES[chosenSubscription],
-        baseUrl: 'http://localhost:3000/profile/settings?tab=account-management&success=true'
-      }).unwrap();
-
-      if (response?.url) {
-        setPaymentUrl(response.url);
-        setShowConfirmModal(true);
-      }
-    } catch (err) {
-      console.error('Error subscription:', err);
-    }
+  const handleSubscribe = () => {
+    subscribe({ chosenSubscription, paymentMerchant: 'STRIPE' });
+    setShowConfirmModal(true);
   };
   return (
-    <>
-      <div className={s.wrapper}>
-        <Button variant={'ghost'} className={s.payment__btn} onClick={handleSubscribe}>
-          <StripeIcon />
-        </Button>
-      </div>
+    <div className={s.wrapper}>
+      <Button variant={'ghost'} className={s.payment__btn} onClick={handleSubscribe}>
+        <StripeIcon />
+      </Button>
       {showConfirmModal && (
         <ConfirmModal setShowModal={setShowConfirmModal} paymentUrl={paymentUrl} isLoading={isLoading} />
       )}
-    </>
+    </div>
   );
 };
